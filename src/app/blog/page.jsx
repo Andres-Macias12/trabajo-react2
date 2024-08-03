0775bc53055036bcb17e50b0dae2a410a96e8606
Steven <steven.mejia@gmail.com>
2024-08-02 19:13:38 -0500

    primer commit

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,15 @@
+export const metadata = {
+    title: 'Super blog',
+    desciptiom: 'Una descripción del sitio',
+    keywords: ['p1', 'p2']
+}
+
+
+
+export default function Post() {
+    return (
+        <div>
+            <h1>Blog de pruebas</h1>
+        </div>
+    )
+}
